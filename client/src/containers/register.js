e3b45d50643c30a719dde1f1955482feb473dac2
Shashwat Tripathi <shashwat.1822csi1045@kiet.edu git config --global user.name Shashwat>
2022-05-22 21:53:42 +0530

    commit from @amishawalia/assignment-chatApp where I was an active collaborator handling the server-side, socket-logic

---
+++ client/src/containers/register.js
@@ -0,0 +1,19 @@
+import SignInOutContainer from "./index.js";
+import './register.css'
+const Register = () => {
+    
+    return <> 
+
+        <div className="wrapper">
+            <div className="left-wrapper">
+                <p className="heading">WELCOME TO THE CHAT APP </p>
+                <img className='logoimg' src="/assets/robo.png" width="400px" padding="50px"></img> 
+            </div>
+            <SignInOutContainer/>
+
+        </div>
+    </>
+};
+
+
+export default Register 
